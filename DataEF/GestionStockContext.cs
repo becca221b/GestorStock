a0bef30a4893f2fdae9c544a09b285238d6b4670
becca221b <lrpmqrp@gmail.com>
2024-06-12 18:57:51 -0300

    Add GestionStockContext with Config

---
+++ DataEF/GestionStockContext.cs
@@ -0,0 +1,31 @@
+﻿using Configuration;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataEF
+{
+    public class GestionStockContext : DbContext
+    {
+        private readonly Config _config;
+        public GestionStockContext(Config config)
+        {
+            _config = config;
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Compra> Compras { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.ConnectionString);
+        }
+    }
+}
