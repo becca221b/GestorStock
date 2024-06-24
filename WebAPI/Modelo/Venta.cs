aed78b6fe3830bf964328ced3bc856ded91ea95f
Marielalcaraz <111131085+Marielalcaraz@users.noreply.github.com>
2024-06-24 13:29:04 -0300

    Create Venta.cs

---
+++ WebAPI/Modelo/Venta.cs
@@ -0,0 +1,10 @@
+namespace WebAPI.Modelo
+{
+    public class Venta
+    {
+        public int ventaid { get; set; }
+        public int productoid { get; set; }
+        public int cant { get; set; }
+        public DateTime fechaVenta { get; set; }
+    }
+}
