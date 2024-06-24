460b05eee4a1d5e8fa3595ec57a8b5979ce1ce22
Marielalcaraz <111131085+Marielalcaraz@users.noreply.github.com>
2024-06-24 13:27:54 -0300

    Create Compra.cs

---
+++ WebAPI/Modelo/Compra.cs
@@ -0,0 +1,11 @@
+namespace WebAPI.Modelo
+{
+    public class Compra
+    {
+        public int compraid { get; set; }
+        public int productoid { get; set; }
+        public int cant { get; set; }
+        public DateTime fechaCompra { get; set; }
+
+    }
+}
